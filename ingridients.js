4799f1fbed20a22015423669530c824952c964e6
killersoul756 <79359982+killersoul756@users.noreply.github.com>
2021-05-08 00:35:27 +0530

    Add files via upload

---
+++ ingridients.js
@@ -0,0 +1,19 @@
+ingridients
+//300 grams boneless  chicken
+//1cup refined flour
+//salt as per your taste (but just dont add a lot or things will get Salty)
+//around 2 tablespoon oil for shallow frying (use any oil like sunflower or soyabean just dont use fish oil because that will make it fishyand i bet youwill not like it) 
+//2 medium onions roughle chopped(roughly like we do our homework)
+//2 medium tomatoes chopped
+//2 teaspoon ginger paste 
+//2 teaspoon garlic paste 
+//2 teaspoon kashmiri red chilli powder(because they aare original you cant trust anyone can you?)
+//1/2 teaspoon tumeric powder
+//2 teaspoon coriandar powder 
+//2 tablespoon lemon juice(pls dont use orange juice) 
+//4 eggs(kindly use chicken eggs i am using crocodiles eggs but i prefer chicken eggs)
+
+forServing
+//2 medium onions sliced
+//8 teaspoons green chutney(how to make it you asked go on youtube you boomer)
+//finally 1 teaspoon vinegar 
---
+++ reciepe.js
@@ -0,0 +1,26 @@
+"mix together the refined flour and salt in a bowl add sufficient water and knead into firmdough"
+"cover with a damp cloth and set aside for about fifteen minutes for it to fermant "
+"divide into eight equal portions and roll into rotis if you dont know how to make a roti sorry but this reciepe i not for you"
+"cook the rotis lightly on a nonstick pan and set aside "
+"heat two table spoons of oil in a non stickpan "
+
+//your time for reading the reciepe has maximized kindly pay 4000 dollars on https://youAreGetiingScamed.com  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//actually i didnt want to type a lot so just get scammed by pressing the above link :)
